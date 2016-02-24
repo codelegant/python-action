820b118b4d2a4e1e173472a3046a7a81ae3e7f14
Laichuanfeng <elegant.lai@kiwifi.cn>
2016-02-24 21:24:11 +0800

    添加：多继承

--- classes/inheritance.py
+++ classes/inheritance.py
@@ -1,6 +1,7 @@
 class Bag:
-    def __init__(self):
+    def __init__(self, x):
         self.data = []
+        self.x = x
 
     def add(self, x):
         self.data.append(x)
@@ -10,13 +11,18 @@ class Bag:
 
 
 class Bag2:
-    def __init__(self):
+    def __init__(self, x):
         self.data = []
+        self.x = x
 
     def showname(self):
         print(self.data)
 
+    def printx(self):
+        print(self.x)
 
+
+# 多继承的两个类的参数必须一致
 class SubBag(Bag, Bag2):
     def addthrice(self, x):
         self.add(x)
@@ -24,7 +30,8 @@ class SubBag(Bag, Bag2):
         self.add(x)
 
 
-m = SubBag()
+m = SubBag(10)
 m.addthrice(3)
 print(m.getvalue())
 m.showname()
+m.printx()
